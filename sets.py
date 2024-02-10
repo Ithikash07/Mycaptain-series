d5cafe4df704bbfb5b2b9d0838ea11bc8b9f5466
Ithikash07 <150923033+Ithikash07@users.noreply.github.com>
2024-02-10 11:59:16 +0530

    Create sets.py
    This python file is about sets which performs some set operations

---
+++ sets.py
@@ -0,0 +1,6 @@
+E={2,4,6,8,9}
+N={4,7,8,6,5}
+print('The Union of E and N is:',E|N)
+print('The Intersection of E and N is:',E&N)
+print('The Difference of E and N is:',E-N)
+print('The Symmetric difference of E and N is:',E^N)
